221e84aad38c9f66e335f6005abb04677a14e839
ravisharma2191 <ravisharma.bmchrc@gail.com>
2025-12-21 12:10:18 +0530

    initail commit

--- Even-odd.py
+++
@@ -1,7 +0,0 @@
-
-num = int(input("Enter an integer number: "))
-
-if num % 2 == 0:
-    print(f"{num} is an even number.")
-else:
-    print(f"{num} is an odd number.")
--- Greeting.py
+++
@@ -1,6 +0,0 @@
-
-fname = input(f"Enter your first name: ")
-mname= input(f"Enter your middle name: ")
-lname= input(f"Enter your last name: ")
-#
-print(f"Hello,{fname} {mname} {lname}! ,"" Welcome to the python program.")
