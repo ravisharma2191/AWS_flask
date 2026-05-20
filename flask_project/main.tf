14bb61d5258bb7e9e7203dc58ebeb11229d1eb07
ravisharma2191 <ravisharma.bmchrc@gail.com>
2026-05-20 17:54:05 +0530

    Tarrafrom

--- flask_project/main.tf
+++ flask_project/main.tf
@@ -38,7 +38,7 @@ data "aws_ami" "ubuntu" {
 # EC2 Instance
 resource "aws_instance" "EC2_RAVI" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [var.security_group_id]
   key_name                    = var.key_name
