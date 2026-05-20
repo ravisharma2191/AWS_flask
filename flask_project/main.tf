2660df10f096402aa3ccb7ab369643616ec0aebe
ravisharma2191 <ravisharma.bmchrc@gail.com>
2026-05-20 17:43:19 +0530

    Tarrafrom

--- flask_project/main.tf
+++ flask_project/main.tf
@@ -9,43 +9,51 @@ variable "vpc_id" {
 
 # Existing Public Subnet
 variable "subnet_id" {
-  default = "subnet-xxxxxxxx"
+  default = "subnet-08d813a8e991ed72e"
 }
 
 # Existing Security Group
 variable "security_group_id" {
-  default = "sg-xxxxxxxx"
+  default = "sg-02213b04ec685f006"
 }
 
+# Key Pair
+variable "key_name" {
+  description = "EC2 Key Pair"
+  type        = string
+}
 
-
-# Latest Amazon Linux 2 AMI
-data "aws_ami" "amazon_linux" {
+# Ubuntu 22.04 AMI
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
+  owners      = ["099720109477"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
 # EC2 Instance
 resource "aws_instance" "EC2_RAVI" {
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t2.micro"
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = [var.security_group_id]
-  key_name               = var.key_name
-
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.security_group_id]
+  key_name                    = var.key_name
   associate_public_ip_address = true
 
   tags = {
-    Name = "Ravi-Web-Server"
+    Name = "Ravi-Ubuntu-Server"
   }
 }
 
 # Output
 output "instance_public_ip" {
- value = aws_instance.EC2_RAVI.public_ip
+  value = aws_instance.EC2_RAVI.public_ip
 }
